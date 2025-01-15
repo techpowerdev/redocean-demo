85ab29978f0052af59b2ebfbf8fa0ded19085f3b
techpowerdevpro <techpowerdevpro@gmail.com>
2025-01-15 11:21:14 +0700

    remove create payment intent from checkout page

--- app/(home)/checkout/page.tsx
+++ app/(home)/checkout/page.tsx
@@ -86,17 +86,14 @@
 
 "use client";
 
-import { useState, useEffect, useCallback } from "react";
+import { useState, useCallback } from "react";
 import { loadStripe, StripeElementsOptions } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import StripeCheckoutForm from "@/app/features/checkout/forms/StripeCheckoutForm";
 import AddressSelector from "@/app/features/address/forms/AddressSelector";
 import Container from "@/components/shared/Container";
-import { useCartServerStore } from "@/state-stores/cartServerStore";
 import { useCheckoutStore } from "@/state-stores/useCheckoutStore";
 import PaymentSuccess from "@/app/features/checkout/forms/PaymentSuccess";
-import { StripeCreatePaymentIntentForOneProductType } from "@/types/stripeTypes";
-import { StripeCreatePaymentIntentForOneProduct } from "@/services/stripeServices";
 
 // Load Stripe with your publishable key
 const stripePromise = loadStripe(
@@ -105,72 +102,10 @@ const stripePromise = loadStripe(
 
 export default function Page(): JSX.Element {
   // global state
-  const cart = useCartServerStore((state) => state.cart);
-  const setPaymentIntent = useCheckoutStore((state) => state.setPaymentIntent);
   const clientSecret = useCheckoutStore((state) => state.clientSecret);
-  const setClientSecret = useCheckoutStore((state) => state.setClientSecret);
-  const setTotalAmount = useCheckoutStore((state) => state.setTotalAmount);
 
   // const [clientSecret, setClientSecret] = useState<string | null>(null);
   const [paymentSuccess, setPaymentSuccess] = useState(false);
-  const [loading, setLoading] = useState(false);
-
-  const createPaymentIntentData = {
-    currency: "thb",
-    captureLater: true,
-    paymentState: "initial_payment",
-    orderId: "order1",
-    orderType: "groupbuying",
-    orderItems: [
-      {
-        unitPrice: 100,
-        discount: 10,
-        quantity: 2,
-      },
-      {
-        unitPrice: 20,
-        discount: 1,
-        quantity: 2,
-      },
-    ],
-  };
-
-  const createPaymentIntent = async (
-    createPaymentIntentData: StripeCreatePaymentIntentForOneProductType
-  ) => {
-    try {
-      const { data } = await StripeCreatePaymentIntentForOneProduct(
-        createPaymentIntentData
-      );
-      console.log(data.paymentIntent);
-      setPaymentIntent(data);
-      if (data.paymentIntent.amount) {
-        const totalAmount = (data.paymentIntent.amount ?? 0) / 100;
-        setTotalAmount(totalAmount);
-      }
-      // console.log(data.order);
-      // if (data.order) {
-      //   setTotalAmount(data.order.totalAmount);
-      // }
-
-      if (data.paymentIntent.client_secret) {
-        setClientSecret(data.paymentIntent.client_secret);
-      }
-    } catch (error) {
-      console.error("Error creating payment intent:", error);
-    }
-  };
-  // Create PaymentIntent on component mount
-  useEffect(() => {
-    // if (cart?.cartItems) {
-    //   // setLoading(true);
-    //   // await createPaymentIntentForCartProducts(cart?.id);
-    //   // setLoading(false);
-    //   console.log("cart product", cart);
-    // } else {
-    createPaymentIntent(createPaymentIntentData);
-    // }
-  }, []);
 
   const options: StripeElementsOptions = {
     clientSecret: clientSecret ?? "",
@@ -178,6 +113,7 @@ export default function Page(): JSX.Element {
     appearance: {
       theme: "stripe",
     },
+    loader: "auto",
   };
 
   const handleSetPaymentSuccess = useCallback((value: boolean) => {
