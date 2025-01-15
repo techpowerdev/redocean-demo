dac18d6c203a248ea6e715b37b9df595c0d690a7
techpowerdevpro <techpowerdevpro@gmail.com>
2025-01-15 11:25:49 +0700

    separate create payment intent function between create payment intent directly when click buynow (one product) and create from cart (for cart Products)

--- services/stripeServices.ts
+++ services/stripeServices.ts
@@ -3,6 +3,7 @@ import apiClient from "./apiClient";
 import {
   StripeCreatePaymentIntentResultType,
   StripeCreatePaymentIntentForOneProductType,
+  StripeCreatePaymentIntentForCartProductsType,
 } from "@/types/stripeTypes";
 
 export async function StripeCreatePaymentIntentForOneProduct(
@@ -10,7 +11,27 @@ export async function StripeCreatePaymentIntentForOneProduct(
 ): Promise<{ data: StripeCreatePaymentIntentResultType; message: string }> {
   try {
     const response = await apiClient.post(
-      `/stripe/payment_intents`,
+      `/stripe/payment_intents/for-one-product`,
+      CreatePaymentIntentData
+    );
+    console.log("response==>", response.data);
+    return response.data; // ดึง data จาก axios แล้วส่งเฉพาะข้อมูลที่ได้รับจาก API
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(
+        error.response?.data?.message || "สร้างรายการชำระเงินไม่สำเร็จ"
+      );
+    }
+    throw new Error("เกิดข้อผิดพลาดบางอย่าง");
+  }
+}
+
+export async function StripeCreatePaymentIntentForCartProduct(
+  CreatePaymentIntentData: StripeCreatePaymentIntentForCartProductsType
+): Promise<{ data: StripeCreatePaymentIntentResultType; message: string }> {
+  try {
+    const response = await apiClient.post(
+      `/stripe/payment_intents/for-cart-products`,
       CreatePaymentIntentData
     );
     console.log("response==>", response.data);
