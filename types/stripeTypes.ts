05d0b50a27ec34369b8b43fa884903b8a67cc2ac
techpowerdevpro <techpowerdevpro@gmail.com>
2025-01-15 23:39:19 +0700

    update stripeType

--- types/stripeTypes.ts
+++ types/stripeTypes.ts
@@ -1,18 +1,32 @@
-export type StripeOrderItemType = {
-  unitPrice: number;
-  discount: number;
+import { ShippingAddressType } from "@/types/addressTypes";
+
+type PaymentStateType = "initial_payment" | "additional_payment"; //  การจ่ายเงินครั้งแรก ,การจ่ายเงินเพิ่มเติม
+
+export type StripeCreatePaymentIntentProductItemType = {
+  productId: string;
+  sku: string;
   quantity: number;
+  promotionActivityId?: string | null;
+  promotionType?: string | null;
+};
+export type StripeCreatePaymentIntentType = {
+  items: StripeCreatePaymentIntentProductItemType[];
+  shippingAddress: ShippingAddressType;
+  // promotionCode?: string;
+  currency?: string;
+  captureLater?: boolean;
+  paymentState?: PaymentStateType;
 };
 
 export type StripeCreatePaymentIntentForOneProductType = {
   productId: string;
   sku: string;
   quantity: number;
-  promotionActivityId?: string;
+  promotionActivityId?: string | null;
   // promotionCode?: string;
   currency?: string;
   captureLater?: boolean;
-  // paymentState: string;
+  paymentState: "initial_payment" | "additional_payment";
 };
 
 export type StripeCreatePaymentIntentForCartProductsType = {
@@ -25,7 +39,7 @@ export type StripeCreatePaymentIntentForCartProductsType = {
   // promotionCode?: string;
   currency?: string;
   captureLater?: boolean;
-  // paymentState: string; // move to check from metadata in stripe instead
+  paymentState: PaymentStateType;
 };
 
 export type StripeCreatePaymentIntentResultType = {
