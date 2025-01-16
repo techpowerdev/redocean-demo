9050a9641e86d964eb1cda24bd485b7f1a177697
techpowerdevpro <techpowerdevpro@gmail.com>
2025-01-16 23:36:20 +0700

    Combobox in a form in a dialog isn't working

--- app/(admin)/admin/event/components/forms/SearchProduct.tsx
+++ app/(admin)/admin/event/components/forms/SearchProduct.tsx
@@ -84,3 +84,139 @@
 //     </>
 //   );
 // }
+"use client";
+
+import * as React from "react";
+import { Check, ChevronsUpDown, CirclePlus } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+
+const frameworks = [
+  {
+    value: "next.js",
+    label: "Next.js",
+  },
+  {
+    value: "sveltekit",
+    label: "SvelteKit",
+  },
+  {
+    value: "nuxt.js",
+    label: "Nuxt.js",
+  },
+  {
+    value: "remix",
+    label: "Remix",
+  },
+  {
+    value: "astro",
+    label: "Astro",
+  },
+];
+
+export function SearchProduct() {
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState("");
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="bg-primary flex gap-2 items-center px-3">
+          <CirclePlus />
+          เพิ่มกิจกรรม
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[640px] h-full p-0">
+        {/* <VisuallyHidden> */}
+        <DialogTitle className="hidden">เพิ่มกิจกรรม</DialogTitle>
+        {/* </VisuallyHidden> */}
+        <DialogDescription className="hidden">
+          This action cannot be undone. This will permanently delete your
+          account and remove your data from our servers.
+        </DialogDescription>
+        <Card className="w-full shadow-none border-none">
+          <CardHeader className="pb-0 px-10">
+            <h1 className="text-xl font-bold">สร้างกิจกรรม</h1>
+          </CardHeader>
+          <Separator />
+          <CardContent className="pb-0">
+            <Popover open={open} onOpenChange={setOpen}>
+              <PopoverTrigger asChild>
+                <Button
+                  variant="outline"
+                  role="combobox"
+                  aria-expanded={open}
+                  className="w-[200px] justify-between"
+                >
+                  {value
+                    ? frameworks.find((framework) => framework.value === value)
+                        ?.label
+                    : "Select framework..."}
+                  <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent
+                className="w-[200px] p-0"
+                // style={{ pointerEvents: "auto" }}
+              >
+                <Command>
+                  <CommandInput placeholder="Search framework..." />
+                  <CommandList>
+                    <CommandEmpty>No framework found.</CommandEmpty>
+                    <CommandGroup>
+                      {frameworks.map((framework) => (
+                        <CommandItem
+                          key={framework.value}
+                          value={framework.value}
+                          onSelect={(currentValue) => {
+                            setValue(
+                              currentValue === value ? "" : currentValue
+                            );
+                            setOpen(false);
+                          }}
+                        >
+                          <Check
+                            className={cn(
+                              "mr-2 h-4 w-4",
+                              value === framework.value
+                                ? "opacity-100"
+                                : "opacity-0"
+                            )}
+                          />
+                          {framework.label}
+                        </CommandItem>
+                      ))}
+                    </CommandGroup>
+                  </CommandList>
+                </Command>
+              </PopoverContent>
+            </Popover>
+          </CardContent>
+        </Card>
+      </DialogContent>
+    </Dialog>
+  );
+}
