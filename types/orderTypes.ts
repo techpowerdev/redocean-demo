bfce4cc947db1514f51edd83e97bca0cbc121fb4
techpowerdevpro <techpowerdevpro@gmail.com>
2025-02-02 21:37:02 +0700

    update order type

--- types/orderTypes.ts
+++ types/orderTypes.ts
@@ -9,7 +9,11 @@ export type OrderType = {
   creditCardFee?: number | null;
   shippingFee?: number | null;
   totalAmount: number;
-  returnAmount: number;
+  totalDiscount: number;
+  netAmount: number;
+  extraPayAmount?: number | null;
+  payAmount?: number | null;
+  returnAmount?: number | null;
   status: string;
   trackingNumber?: string | null;
   shippingAddress: {
@@ -29,6 +33,7 @@ export type OrderType = {
 
   user?: UserType | null;
   orderItems?: OrderItemType[] | null;
+  payments?: PaymentItemType[] | null;
 };
 
 export type OrderItemType = {
@@ -36,10 +41,8 @@ export type OrderItemType = {
   sku: string;
   quantity: number;
   unitPrice: number;
-  discount?: number | null;
-  discountType?: string | null;
-  // specialDiscount?: number | null;
-  total: number;
+  discount: number;
+  // total: number;
   createdAt: Date;
   updatedAt: Date;
 
@@ -57,6 +60,20 @@ export type OrderItemType = {
   image?: string;
 };
 
+export type PaymentItemType = {
+  id: string;
+  amount: number;
+  amountRefunded: number;
+  captured: boolean;
+  chargeId: string | null;
+  orderId: string;
+  paymentState: string;
+  paymentStatus: string;
+  paymentType: string | null;
+  stripePaymentId: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
 // end of types based on the prisma schema
 
 // types for the client side
@@ -97,19 +114,101 @@ export type CreateOderItemType = {
   quantity: number;
   unitPrice: number;
   discount?: number | null;
-  // specialDiscount: number | null;
+  total: number;
   productId: string;
   promotionActivityId?: string | null;
-  total: number;
 };
 
 export type OrderSummaryType = {
   totalOrders: number;
   pendingOrders: number;
-  shippingOrders: number;
-  completedOrders: number;
+  awaitingConfirmationOrders: number;
+  confirmedOrders: number;
+  preparingToShipOrders: number;
+  cancelledOrders: number;
 };
+
 export type OrderSummaryReponseType = {
   message: string | null;
   data: OrderSummaryType;
 };
+
+// refractor type
+
+// ลงท้ายด้วย param/params หากเป็น type ของ input ใน function
+export interface CreateOrderWithPaymentIntentParams {
+  items: CreateOrderItem[];
+  shippingAddress: Address;
+  currency?: string;
+  captureLater?: boolean;
+  paymentState?: PaymentState;
+}
+
+export interface CreateOrderItem {
+  productId: string;
+  sku: string;
+  quantity: number;
+  promotionActivityId?: string | null;
+  promotionType?: string | null;
+}
+
+export interface Address {
+  // id: string;
+  recipient: string;
+  phoneNumber: string;
+  address: string;
+  street: string;
+  subDistrict: string;
+  district: string;
+  province: string;
+  postalCode: string;
+  // isActive: boolean;
+  // createdAt: Date;
+  // updatedAt: Date;
+
+  // userId: string;
+
+  // user?: UserType | null;
+}
+
+export type PaymentState = "initial_payment" | "additional_payment";
+export interface AddressResponse extends Address {
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
+  userId: string;
+  user?: UserType | null;
+}
+
+export interface CreateOrderWithPaymentIntentResponse {
+  paymentIntent: {
+    id: string;
+    client_secret: string;
+    status: string;
+    amount: number;
+    currency: string;
+    metadata: Record<string, string>;
+  };
+  items: {
+    productId: string;
+    sku: string;
+    name: string;
+    description: string;
+    variantOptions: string;
+    image: string;
+    promotionActivityId?: string | null;
+    promotionType?: string | null;
+    unitPrice: number;
+    quantity: number;
+    discount: number;
+    discountedPrice: number;
+    total: number;
+    warningMessage?: string | null;
+    isAvailableStock: boolean;
+  }[];
+  totalAmount: number;
+  totalDiscount: number;
+  netAmount: number;
+  totalQuantity: number;
+  nextProcess: boolean;
+}
