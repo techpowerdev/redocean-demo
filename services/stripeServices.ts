dc11555c788884d126f60240c588c90390a710f7
techpowerdevpro <techpowerdevpro@gmail.com>
2025-02-02 21:38:46 +0700

    update stripe service

--- services/stripeServices.ts
+++ services/stripeServices.ts
@@ -5,7 +5,48 @@ import {
   StripeCreatePaymentIntentForOneProductType,
   StripeCreatePaymentIntentForCartProductsType,
   StripeCreatePaymentIntentType,
+  CreatePaymentIntentResponse,
+  CreatePaymentIntentParam,
 } from "@/types/stripeTypes";
+
+export async function createPaymentIntent(
+  CreateData: CreatePaymentIntentParam
+): Promise<{ data: CreatePaymentIntentResponse; message: string }> {
+  try {
+    const response = await apiClient.post(
+      `/stripe/payment_intents`,
+      CreateData
+    );
+    console.log("response==>", response.data);
+    return response.data; // ดึง data จาก axios แล้วส่งเฉพาะข้อมูลที่ได้รับจาก API
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(
+        error.response?.data?.message || "สร้างรายการชำระเงินไม่สำเร็จ"
+      );
+    }
+    throw new Error("เกิดข้อผิดพลาดบางอย่าง");
+  }
+}
+
+export async function getPaymentIntentById(
+  paymentIntentId: string
+): Promise<{ data: any }> {
+  try {
+    const response = await apiClient.get(
+      `/stripe/payment_intents/${paymentIntentId}`
+    );
+    console.log("get payment intent==>", response.data);
+    return response.data; // ดึง data จาก axios แล้วส่งเฉพาะข้อมูลที่ได้รับจาก API
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(
+        error.response?.data?.message || "สร้างรายการชำระเงินไม่สำเร็จ"
+      );
+    }
+    throw new Error("เกิดข้อผิดพลาดบางอย่าง");
+  }
+}
 
 export async function StripeCreatePaymentIntent(
   CreatePaymentIntentData: StripeCreatePaymentIntentType
