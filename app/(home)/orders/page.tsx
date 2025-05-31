b39f37ca55ad81163a7a1749e52d86a67c21fe89
techpowerdevpro <techpowerdevpro@gmail.com>
2025-05-31 13:38:57 +0700

    refactor: clean up commented code and simplify Orders component layout

--- app/(home)/orders/page.tsx
+++ app/(home)/orders/page.tsx
@@ -1,29 +1,25 @@
 "use client";
 
 import OrderList from "@/app/features/order/OrderList";
-import { useCurrentUserStore } from "@/state-stores/useCurrentUserStore";
-import LineLogin from "@/app/features/auth/LineLogin";
+// import { useCurrentUserStore } from "@/state-stores/useCurrentUserStore";
+// import LineLogin from "@/app/features/auth/LineLogin";
 import Heading from "@/components/shared/Heading";
 import { FileClock } from "lucide-react";
 import Container from "@/components/shared/Container";
 
 export default function Orders() {
-  const currentUser = useCurrentUserStore((state) => state.currentUser);
+  // const currentUser = useCurrentUserStore((state) => state.currentUser);
 
-  if (!currentUser) {
-    return <LineLogin />;
-  }
+  // if (!currentUser) {
+  //   return <LineLogin />;
+  // }
 
   return (
     <Container>
       {/* <MobileContainer> */}
-      <div className="flex justify-between items-center gap-4 py-2 mb-2 ">
-        <Heading title="ประวัติการสั่งซื้อ" center />
-        <FileClock size={30} className="text-gray-300" />
-      </div>
-      <div className="flex flex-col space-y-2">
-        <OrderList />
-      </div>
+
+      <OrderList />
+
       {/* </MobileContainer> */}
     </Container>
   );
