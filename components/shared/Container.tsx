5fb74c713cbc57e884597723ee322e97f362a359
techpowerdevpro <techpowerdevpro@gmail.com>
2025-05-31 13:14:31 +0700

    refractor : Container components

--- components/shared/Container.tsx
+++ components/shared/Container.tsx
@@ -1,8 +1,15 @@
 interface ContainerProps {
   children: React.ReactNode;
+  className?: string;
 }
-export default function Container({ children }: ContainerProps) {
+
+export default function Container({
+  children,
+  className = "",
+}: ContainerProps) {
   return (
-    <div className="max-w-screen-xl mx-auto px-2 sm:px-10">{children}</div>
+    <div className={`max-w-screen-xl mx-auto px-2 sm:px-10 ${className}`}>
+      {children}
+    </div>
   );
 }
