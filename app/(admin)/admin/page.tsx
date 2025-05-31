f5f34abca7db3f9b5c5f8b211bde4a8d67127dae
techpowerdevpro <techpowerdevpro@gmail.com>
2025-05-31 13:34:12 +0700

    refactor: update dashboard to use getPromotions and improve event display

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -1,20 +1,37 @@
-import ShowEventCard from "../../features/dashboard/ShowEventCard";
-import { getPromotionToday } from "@/services/promotionServices";
+import { getPromotions } from "@/services/promotionServices";
 import NoPromotion from "@/app/features/promotion/NoPromotion";
+import PromotionOrderSummary from "@/app/features/dashboard/PromotionOrderSummary";
+import { Separator } from "@/components/ui/separator";
+import FlashSaleHeader from "@/app/features/flash-sale/FlashSaleHeader";
+import GroupBuyHeader from "@/app/features/flash-sale/GroupBuyHeader";
 
 export default async function dashboard() {
-  const promotions = await getPromotionToday();
+  const promotions = await getPromotions("today");
 
   if (!promotions?.data || promotions.data.length === 0) {
     return <NoPromotion />;
   }
 
   return (
-    <div className="flex-grow p-6 md:overflow-y-auto md:px-12 md:py-6">
-      <h1 className={`mb-4 text-xl md:text-2xl`}>กิจกรรมวันนี้</h1>
+    <div className="flex-grow p-2 md:overflow-y-auto md:p-4">
+      <h1 className="mb-4 text-xl md:text-2xl">กิจกรรมวันนี้</h1>
       {promotions.data?.map((promotion) => (
         <div key={promotion.id}>
-          <ShowEventCard promotion={promotion} />
+          <div className="w-full flex gap-2 justify-between items-center">
+            {promotion.type === "flashsale" ? (
+              <FlashSaleHeader promotion={promotion} />
+            ) : (
+              <GroupBuyHeader promotion={promotion} />
+            )}
+          </div>
+          {promotion.promotionActivities?.map((promotionActivity) => (
+            <div key={promotionActivity.id}>
+              {/* <ShowEventCard promotion={promotionActivity.promotion} /> */}
+              {promotionActivity.promotion?.startAt}
+              <PromotionOrderSummary promotionActivity={promotionActivity} />
+            </div>
+          ))}
+          <Separator className="my-6" />
         </div>
       ))}
     </div>
