5c20487bc39f7af610bcda1d7159996e41544848
techpowerdevpro <techpowerdevpro@gmail.com>
2025-06-01 17:49:54 +0700

    refactor: convert ProfileDetail to use useEffect for fetching user data

--- app/(home)/profile/[id]/page.tsx
+++ app/(home)/profile/[id]/page.tsx
@@ -1,20 +1,36 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
 import { EditProfileForm } from "@/app/features/profile/forms/EditProfileForm";
 import { getCurrentUser } from "@/services/authServices";
+import { User } from "@/types/baseTypes";
 
 type Props = {
   params: { id: string };
 };
+export default function ProfileDetail({ params }: Props) {
+  const [profile, setProfile] = useState<User | null>(null);
 
-export default async function page({ params }: Props) {
-  const profile = await getCurrentUser();
+  useEffect(() => {
+    const fetchProfile = async () => {
+      // @@@@@ getCurrentUser ถูก สร้างจาก authAxios ซึ่งมีการเรียกใช้ Server action ภายใน
+      const profileResponse = await getCurrentUser();
+      setProfile(profileResponse.data);
+    };
+    fetchProfile();
+  }, [params.id]);
 
-  if (!profile || !profile.data) {
-    <div>ไม่พบข้อมูลผู้ใช้งาน</div>;
+  if (!profile) {
+    return (
+      <div className="w-full flex justify-center items-center text-red-500 mt-4">
+        ไม่พบข้อมูลผู้ใช้งาน
+      </div>
+    );
   }
 
   return (
     <div className="max-w-screen-sm mx-auto p-4 rounded-md shadow-md mt-4">
-      <EditProfileForm user={profile.data} />
+      <EditProfileForm user={profile} />
     </div>
   );
 }
