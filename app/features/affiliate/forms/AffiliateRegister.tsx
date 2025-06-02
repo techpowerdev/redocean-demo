f11f5ff6bf728fd485da9523cf8970d1c8e477eb
techpowerdevpro <techpowerdevpro@gmail.com>
2025-06-03 01:37:34 +0700

    refactor: update useEffect dependencies to include payoutChannels

--- app/features/affiliate/forms/AffiliateRegister.tsx
+++ app/features/affiliate/forms/AffiliateRegister.tsx
@@ -153,7 +153,7 @@ export default function AffiliateRegister() {
     } else {
       form.setValue("channelCode", "");
     }
-  }, [selectedBankName, form.setValue]);
+  }, [selectedBankName, form, payoutChannels]);
 
   return (
     <div className="container mx-auto flex flex-col items-center justify-start min-h-screen py-12 px-4 md:px-6">
