f9e8d9120a9b3fbd729696e89597b683aa4b4c20
techpowerdevpro <techpowerdevpro@gmail.com>
2025-06-02 23:15:38 +0700

    refactor middleware to improve role checking and path handling

--- middleware.ts
+++ middleware.ts
@@ -3,12 +3,21 @@ import { getSession } from "./lib/session";
 
 export default async function middleware(req: NextRequest) {
   const session = await getSession();
+
   if (!session || !session.user)
     return NextResponse.redirect(new URL("/login", req.nextUrl));
 
+  const role = session.user.role;
+
+  const { pathname } = req.nextUrl;
+  console.log("pathname:", pathname);
+
+  const isAdminPage =
+    pathname.startsWith("/admin") || pathname.startsWith("/affiliate/admin");
+
   // Check user role for /admin route
-  if (req.nextUrl.pathname.startsWith("/admin")) {
-    if (session.user.role !== "admin") {
+  if (isAdminPage) {
+    if (role !== "admin") {
       return NextResponse.redirect(new URL("/", req.nextUrl));
     }
   }
@@ -18,6 +27,8 @@ export default async function middleware(req: NextRequest) {
 
 export const config = {
   // ถ้าไม่มี session login เมื่ออยู่ใน path เหล่านี้จะถูก redirect ไป "/login"
+
+  // กำหนดว่าจะให้ middleware ทำงานที่ path ไหนบ้าง (ต้องกำหนด ไม่งั้น middleware จะไม่ส่งผลใน path นั้นๆ)
   matcher: [
     "/cart",
     "/orders",
