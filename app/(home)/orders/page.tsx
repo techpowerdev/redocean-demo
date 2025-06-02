814e6e30e47f18a4cf281ef350d7dd6b235ce32e
techpowerdevpro <techpowerdevpro@gmail.com>
2025-06-03 01:33:03 +0700

    refactor: remove unused currentUser logic and LineLogin component from Orders page

--- app/(home)/orders/page.tsx
+++ app/(home)/orders/page.tsx
@@ -1,19 +1,9 @@
 "use client";
 
 import OrderList from "@/app/features/order/OrderList";
-// import { useCurrentUserStore } from "@/state-stores/useCurrentUserStore";
-// import LineLogin from "@/app/features/auth/LineLogin";
-import Heading from "@/components/shared/Heading";
-import { FileClock } from "lucide-react";
 import Container from "@/components/shared/Container";
 
 export default function Orders() {
-  // const currentUser = useCurrentUserStore((state) => state.currentUser);
-
-  // if (!currentUser) {
-  //   return <LineLogin />;
-  // }
-
   return (
     <Container>
       {/* <MobileContainer> */}
